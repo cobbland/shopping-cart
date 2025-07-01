c2867988a01c7caf2d1a814e02d5599d38e068d4
cobbland <jacobdensford@proton.me>
2025-07-01 07:55:04 -0400

    Add loading

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import Card from "./Card.jsx";
+import Loading from "./Loading.jsx";
 
 export default function Shop() {
-    const [shopItems, setShopItems] = useState([]);
+    const [shopItems, setShopItems] = useState(null);
 
     useEffect(() => {
         async function dataFetch() {
@@ -16,7 +17,7 @@ export default function Shop() {
         <div>
             <h2>Shop</h2>
             <p>This is the shop.</p>
-            {shopItems.map((item) => (
+            {!shopItems ? <Loading /> : shopItems.map((item) => (
                 <Card 
                     title={item.title} 
                     price={item.price} 
